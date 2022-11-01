a96830d4f4b21953a44ea64a6310ffff59f2d685
Åsa Jonsson <Asa.Jonsson@lexicon.se>
2022-11-01 12:30:43 +0100

    Add project files.

---
+++ Gr44MVCEmpty/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gr44MVCEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gr44MVCEmpty/Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace Gr44MVCEmpty.Models
+{
+    public class Doctor
+    {
+        public static string Diagnose(double temp)
+        {
+            return "";
+        }
+    }
+}
